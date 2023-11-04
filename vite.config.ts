affd12ddfa10bdb2ed36a08937fdf5cafccf65d0
SZ7M8ci7 <83046414+SZ7M8ci7@users.noreply.github.com>
2023-11-04 13:33:04 +0900

    route

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-  ? "twst"
-  : "./",
+  base:"twst",
   plugins: [
     vue({
       template: { transformAssetUrls }
