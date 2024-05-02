e6b8657423c2b4752eec90b8b0ad9e2948b18487
SZ7M8ci7 <SZ7M8ci7@users.noreply.github.com>
2024-05-02 20:17:56 +0000

    Updates

--- assets/VCardText-180e6007.js
+++
@@ -1,1 +0,0 @@
-import{au as t}from"./index-2b49b7e7.js";const e=t("v-card-title"),r=t("v-card-text");export{e as V,r as a};
---
+++ assets/VCardText-c58696a8.js
@@ -0,0 +1,1 @@
+import{au as t}from"./index-472e82d7.js";const e=t("v-card-title"),r=t("v-card-text");export{e as V,r as a};
