8f6306ec826706588c1b863fbfe320e90d5f1cb0
38-bgc <38-bgc@users.noreply.github.com>
2024-12-13 22:27:42 +0900

    Co-authored-by: SZ7M8ci7 <SZ7M8ci7@users.noreply.github.com>

---
+++ src/views/search1.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container style="min-width: 80%;">
+    <div>
+      <SearchHeader @search-started="handleSearchStarted" />
+    </div>
+    <v-tabs v-model="tab" color="indigo-darken-2" fixed-tabs
+      :show-arrows="false"
+      :mobile-breakpoint="Infinity">
+      <v-tab value="search">{{ $t('search.characterList') }}</v-tab>
+      <v-tab value="result">{{ $t('search.result') }}</v-tab>
+    </v-tabs>
+    <v-card-text>
+      <v-window v-model="tab" disabled >
+        <v-window-item value="search">
+          <SearchBody />
+        </v-window-item>
+        <v-window-item value="result">
+          <ResultBody/>
+        </v-window-item>
+      </v-window>
+    </v-card-text>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from 'vue';
+import SearchHeader from '@/components/SearchHeader.vue';
+import SearchBody from '@/components/SearchBody.vue';
+import ResultBody from '@/components/ResultBody.vue';
+import {useCharacterStore} from '@/store/characters';
+
+const characterStore = useCharacterStore();
+
+const tab = ref('search');
+const searchStarted = ref(false);
+
+const handleSearchStarted = () => {
+  tab.value = 'result'; // タブを検索結果に切り替える
+  searchStarted.value = true; // 検索が開始されたことを示す
+};
+
+onMounted(() => {
+  characterStore.handlePageChange('searchPage');
+});
+</script>
