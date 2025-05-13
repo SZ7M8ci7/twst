7ac1a18613c5c5b3b4f24f4520a2032998b735c6
SZ7M8ci7 <SZ7M8ci7@users.noreply.github.com>
2025-05-13 13:35:34 +0000

    Updates

--- assets/VCardText-a18a7257.js
+++
@@ -1,1 +0,0 @@
-import{bb as t}from"./index-861c64e1.js";const e=t("v-card-title"),r=t("v-card-text");export{r as V,e as a};
---
+++ assets/VSpacer-d15a176b.js
@@ -0,0 +1,1 @@
+import{bb as a}from"./index-84205fe4.js";const r=a("v-spacer","div","VSpacer");export{r as V};
